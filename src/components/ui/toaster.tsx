507a218618a6a465d3cf5c302187861409e50603
Mirsaid Mirakhmedov <mir21.07.2004@gmail.com>
2026-04-01 18:41:14 +0500

    fix: toaster width

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -19,7 +19,7 @@ export const Toaster = () => {
     <Portal>
       <ChakraToaster toaster={toaster} insetInline={{ mdDown: "4" }}>
         {(toast) => (
-          <Toast.Root width={{ md: "sm" }}>
+          <Toast.Root width="auto" minW="300px" maxW="400px">
             {toast.type === "loading" ? (
               <Spinner size="sm" color="blue.solid" />
             ) : (
